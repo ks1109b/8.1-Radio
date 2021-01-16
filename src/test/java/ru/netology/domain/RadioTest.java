514e4327963a90b5bb40db8e37f1b19952890e03
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-01-16 16:59:09 +0300

    Added packages

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,3 +1,6 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +10,11 @@ class RadioTest {
     @Test
     public void shouldSetStation() {
         Radio radio = new Radio();
-        assertEquals(0, radio.getCurrentStation());
+        Assertions.assertEquals(0, radio.getCurrentStation());
         radio.setCurrentStation(5);
-        assertEquals(5, radio.getCurrentStation());
+        Assertions.assertEquals(5, radio.getCurrentStation());
         radio.setCurrentStation(9);
-        assertEquals(9, radio.getCurrentStation());
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
@@ -19,25 +22,25 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setCurrentStation(9);
         radio.setCurrentStation(radio.getCurrentStation()+1);
-        assertEquals(0, radio.getCurrentStation());
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     public void shouldPrevStation() {
         Radio radio = new Radio();
-        assertEquals(0, radio.getCurrentStation());
+        Assertions.assertEquals(0, radio.getCurrentStation());
         radio.setCurrentStation(radio.getCurrentStation()-1);
-        assertEquals(9, radio.getCurrentStation());
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void shouldSetVolume() {
         Radio radio = new Radio();
-        assertEquals(0, radio.getVolume());
+        Assertions.assertEquals(0, radio.getVolume());
         radio.setVolume(5);
-        assertEquals(5, radio.getVolume());
+        Assertions.assertEquals(5, radio.getVolume());
         radio.setVolume(10);
-        assertEquals(10, radio.getVolume());
+        Assertions.assertEquals(10, radio.getVolume());
     }
 
     @Test
@@ -45,7 +48,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(10);
         radio.setVolume(radio.getVolume()+1);
-        assertEquals(10, radio.getVolume());
+        Assertions.assertEquals(10, radio.getVolume());
     }
 
     @Test
@@ -53,7 +56,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(0);
         radio.setVolume(radio.getVolume()-1);
-        assertEquals(0, radio.getVolume());
+        Assertions.assertEquals(0, radio.getVolume());
     }
 }
 
